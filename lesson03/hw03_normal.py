39676da6c12dc8783822fc554466f082d8473ff5
DaharGon <arriec@rambler.ru>
2017-06-28 06:23:47 -0500

    добавил пузырек

--- lesson03/hw03_normal.py
+++ lesson03/hw03_normal.py
@@ -17,10 +17,16 @@ print(fibonacci(5, 9))
 # Для решения данной задачи нельзя использовать встроенную фукцию и метод sort()
 
 
-#def sort_to_max(origin_list):
-    #pass
+def sort_to_max(origin_list):
+    n = 1 
+    while n < len(origin_list):
+         for i in range(len(origin_list)-n):
+              if origin_list[i] > origin_list[i+1]:
+                   origin_list[i],origin_list[i+1] = origin_list[i+1],origin_list[i]
+         n += 1    
+    return origin_list
 
-#sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
 # Задача-3:
 # Напишите собственную реализацию функции filter
